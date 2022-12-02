2d7bba94ff01fe392db6aa96a5067747f1c05d17
Atlant <cuz.den30@gmail.com>
2022-12-02 14:20:14 +0300

    zadanie25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите  цикл, который принимает на вход 2 числа (А и В ) и возводит число А внатуральную степень В. 
+
+int Exponentiate(int num1, int num2)
+{
+    int result=1;
+    for (int i=1; i<=num2; i++)
+    {
+        result*=num1;
+    }
+    return result;
+}
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int tgm= Exponentiate(num1, num2);
+Console.WriteLine(tgm);
+
+
+
