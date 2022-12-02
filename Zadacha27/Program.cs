d7920f27fa2e1f137f46d26dbcd749f3027b39fe
Atlant <cuz.den30@gmail.com>
2022-12-02 16:03:57 +0300

    zadanie27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число и вылает суимму чисел в числе
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetNumb(int num)
+{
+    int result = 0;
+    while(num>10)
+    {   
+        num%=10;
+        result+=num;
+    }
+return result;
+}
+
+
+int result=0;
+while(num>0)
+{
+    if(num<10)
+    {
+        result+=num;
+        break;
+    }
+    else
+    {
+        result+=GetNumb(num);
+        num/=10;
+    }
+
+}
+Console.WriteLine(result);
+    
+
+
+ 
