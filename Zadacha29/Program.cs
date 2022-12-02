cf156078b61ab54da5e1b7a1390279c6d666a778
Atlant <cuz.den30@gmail.com>
2022-12-02 15:22:54 +0300

    zadanie29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран
+
+int []array = new int[8];
+
+void FillArray(int [] arry)
+{
+    for(int i=0; i<arry.Length; i++)
+    {
+        arry[i]= new Random().Next(0,100);
+    }
+}
+void PrintArry(int [] arr)
+{
+    for(int i=0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+FillArray(array);
+PrintArry(array);
+
